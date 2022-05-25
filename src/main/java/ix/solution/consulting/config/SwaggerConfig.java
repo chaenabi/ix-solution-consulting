c75a583f01fe1f93ce1869eb8dab9e690d0950b4
chaenabi <find1086@gmail.com>
2022-05-25 20:44:15 +0900

    add basic front page

--- src/main/java/ix/solution/consulting/config/SwaggerConfig.java
+++ src/main/java/ix/solution/consulting/config/SwaggerConfig.java
@@ -9,6 +9,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -36,6 +37,29 @@ public class SwaggerConfig {
                 .paths(PathSelectors.ant(pathUri))
                 .build()
                 .apiInfo(apiInfo());
+    }
+
+    /**
+     * SwaggerUI information
+     */
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
     }
 
     private ApiInfo apiInfo() {
